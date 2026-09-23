328e330c0fdf6d690013163989ba14e0540e3a77
rickamaral94 <rick.amaral94@gmail.com>
2026-09-23 07:46:06 -0300

    Android: capture thread and Vulkan drop telemetry

--- src/android/app/src/test/java/info/cemu/cemu/common/diagnostics/PerformanceTelemetryTest.kt
+++ src/android/app/src/test/java/info/cemu/cemu/common/diagnostics/PerformanceTelemetryTest.kt
@@ -22,4 +22,15 @@ class PerformanceTelemetryTest {
         assertEquals(50.0, calculateKgslBusyPercent(150L to 300L)!!, 0.001)
         assertNull(calculateKgslBusyPercent(null))
     }
+
+    @Test
+    fun parsesLinuxThreadCpuStatWithSpacesInName() {
+        val stat = "42 (Cemu render worker) S 1 2 3 4 5 6 7 8 9 10 120 30 0 0"
+        assertEquals(ThreadCpuStat(42, "RenderThread", 150), parseProcThreadStat(stat, "RenderThread\n"))
+    }
+
+    @Test
+    fun rejectsIncompleteLinuxThreadCpuStat() {
+        assertNull(parseProcThreadStat("42 (broken) S 1 2", "broken"))
+    }
 }
