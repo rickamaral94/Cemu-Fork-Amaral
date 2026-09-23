aceeaa0bb919eaaf8aedbdf093ce4e3a25a31e31
rickamaral94 <rick.amaral94@gmail.com>
2026-09-23 11:58:52 -0300

    Latte: coalesce nearby buffer uploads

--- src/Cafe/HW/Latte/Core/LattePerformanceMonitor.cpp
+++ src/Cafe/HW/Latte/Core/LattePerformanceMonitor.cpp
@@ -141,7 +141,7 @@ void LattePerformanceMonitor_frameEnd()
 			if (fps < 28.0)
 			{
 				cemuLog_log(LogType::Force,
-					"Cemu performance drop: fps={:.2f} drawCallsPerFrame={} fastDrawCallsPerFrame={} renderCpuMs={:.3f} recompilerLeavesPerSecond={} threadLeavesPerSecond={} indexUploadKiBPerFrame={} vkPipelines={} vkDescriptorSets={} vkImages={} vkImageViews={} vkRenderPasses={} vkFramebuffers={} barriersLastFrame={} inputTextureBarriersLastFrame={} renderPassLoadBarriersLastFrame={} skippedColorFeedbackBarriersLastFrame={} beginRenderPassesLastFrame={} renderPassFboChangesLastFrame={} renderPassSelfDependencySplitsLastFrame={} renderPassReopensSameFboLastFrame={} bufferCache=[initialUploads:{},changedUploads:{},streamoutUploads:{},uploadKiB:{},pagesChecked:{},pagesChanged:{}] renderPassEndsLastFrame=[submit:{},present:{},clear:{},textureTransfer:{},bufferTransfer:{},query:{},readback:{},fboTransition:{},other:{}]",
+					"Cemu performance drop: fps={:.2f} drawCallsPerFrame={} fastDrawCallsPerFrame={} renderCpuMs={:.3f} recompilerLeavesPerSecond={} threadLeavesPerSecond={} indexUploadKiBPerFrame={} vkPipelines={} vkDescriptorSets={} vkImages={} vkImageViews={} vkRenderPasses={} vkFramebuffers={} barriersLastFrame={} inputTextureBarriersLastFrame={} renderPassLoadBarriersLastFrame={} skippedColorFeedbackBarriersLastFrame={} beginRenderPassesLastFrame={} renderPassFboChangesLastFrame={} renderPassSelfDependencySplitsLastFrame={} renderPassReopensSameFboLastFrame={} bufferCache=[initialUploads:{},changedUploads:{},streamoutUploads:{},uploadKiB:{},pagesChecked:{},pagesChanged:{},bridgedPages:{},mergedRuns:{}] renderPassEndsLastFrame=[submit:{},present:{},clear:{},textureTransfer:{},bufferTransfer:{},query:{},readback:{},fboTransition:{},other:{}]",
 					fps, drawCallsPerFrame, fastDrawCallsPerFrame,
 					TimerValueToMilliseconds(performanceMonitor.gpuTime_frameTime), rlps, tlps,
 					(performanceMonitor.stats.indexDataUploadPerFrame + 1023) / 1024,
@@ -165,6 +165,8 @@ void LattePerformanceMonitor_frameEnd()
 					(performanceMonitor.vk.numBufferCacheUploadBytesPerFrame.get() + 1023) / 1024,
 					performanceMonitor.vk.numBufferCachePagesCheckedPerFrame.get(),
 					performanceMonitor.vk.numBufferCachePagesChangedPerFrame.get(),
+					performanceMonitor.vk.numBufferCacheBridgedPagesPerFrame.get(),
+					performanceMonitor.vk.numBufferCacheMergedRunsPerFrame.get(),
 					performanceMonitor.vk.numRenderPassEndsSubmitPerFrame.get(),
 					performanceMonitor.vk.numRenderPassEndsPresentationPerFrame.get(),
 					performanceMonitor.vk.numRenderPassEndsClearPerFrame.get(),
@@ -204,4 +206,6 @@ void LattePerformanceMonitor_frameBegin()
 	performanceMonitor.vk.numBufferCacheUploadBytesPerFrame.reset();
 	performanceMonitor.vk.numBufferCachePagesCheckedPerFrame.reset();
 	performanceMonitor.vk.numBufferCachePagesChangedPerFrame.reset();
+	performanceMonitor.vk.numBufferCacheBridgedPagesPerFrame.reset();
+	performanceMonitor.vk.numBufferCacheMergedRunsPerFrame.reset();
 }
