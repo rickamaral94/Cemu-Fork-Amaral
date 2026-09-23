4450fcd0222af55a5db764aa351dce4b01193cdf
rickamaral94 <rick.amaral94@gmail.com>
2026-09-23 13:36:49 -0300

    Vulkan: bind small Android vertex uploads directly

--- src/Cafe/HW/Latte/Core/LattePerformanceMonitor.cpp
+++ src/Cafe/HW/Latte/Core/LattePerformanceMonitor.cpp
@@ -141,7 +141,7 @@ void LattePerformanceMonitor_frameEnd()
 			if (fps < 28.0)
 			{
 				cemuLog_log(LogType::Force,
-					"Cemu performance drop: fps={:.2f} drawCallsPerFrame={} fastDrawCallsPerFrame={} renderCpuMs={:.3f} recompilerLeavesPerSecond={} threadLeavesPerSecond={} indexUploadKiBPerFrame={} vkPipelines={} vkDescriptorSets={} vkImages={} vkImageViews={} vkRenderPasses={} vkFramebuffers={} barriersLastFrame={} inputTextureBarriersLastFrame={} renderPassLoadBarriersLastFrame={} skippedColorFeedbackBarriersLastFrame={} beginRenderPassesLastFrame={} renderPassFboChangesLastFrame={} renderPassSelfDependencySplitsLastFrame={} renderPassReopensSameFboLastFrame={} bufferCache=[initialUploads:{},changedUploads:{},streamoutUploads:{},uploadKiB:{},pagesChecked:{},pagesChanged:{}] uploadSources=[vertex:{}/{},vsUniform:{}/{},gsUniform:{}/{},psUniform:{}/{}] renderPassEndsLastFrame=[submit:{},present:{},clear:{},textureTransfer:{},bufferTransfer:{},query:{},readback:{},fboTransition:{},other:{}]",
+					"Cemu performance drop: fps={:.2f} drawCallsPerFrame={} fastDrawCallsPerFrame={} renderCpuMs={:.3f} recompilerLeavesPerSecond={} threadLeavesPerSecond={} indexUploadKiBPerFrame={} vkPipelines={} vkDescriptorSets={} vkImages={} vkImageViews={} vkRenderPasses={} vkFramebuffers={} barriersLastFrame={} inputTextureBarriersLastFrame={} renderPassLoadBarriersLastFrame={} skippedColorFeedbackBarriersLastFrame={} beginRenderPassesLastFrame={} renderPassFboChangesLastFrame={} renderPassSelfDependencySplitsLastFrame={} renderPassReopensSameFboLastFrame={} bufferCache=[initialUploads:{},changedUploads:{},streamoutUploads:{},uploadKiB:{},pagesChecked:{},pagesChanged:{}] uploadSources=[vertex:{}/{},vsUniform:{}/{},gsUniform:{}/{},psUniform:{}/{},directVertex:{}/{}] renderPassEndsLastFrame=[submit:{},present:{},clear:{},textureTransfer:{},bufferTransfer:{},query:{},readback:{},fboTransition:{},other:{}]",
 					fps, drawCallsPerFrame, fastDrawCallsPerFrame,
 					TimerValueToMilliseconds(performanceMonitor.gpuTime_frameTime), rlps, tlps,
 					(performanceMonitor.stats.indexDataUploadPerFrame + 1023) / 1024,
@@ -173,6 +173,8 @@ void LattePerformanceMonitor_frameEnd()
 					(performanceMonitor.vk.numBufferCacheGeometryUniformUploadBytesPerFrame.get() + 1023) / 1024,
 					performanceMonitor.vk.numBufferCachePixelUniformUploadsPerFrame.get(),
 					(performanceMonitor.vk.numBufferCachePixelUniformUploadBytesPerFrame.get() + 1023) / 1024,
+					performanceMonitor.vk.numDirectVertexUploadsPerFrame.get(),
+					(performanceMonitor.vk.numDirectVertexUploadBytesPerFrame.get() + 1023) / 1024,
 					performanceMonitor.vk.numRenderPassEndsSubmitPerFrame.get(),
 					performanceMonitor.vk.numRenderPassEndsPresentationPerFrame.get(),
 					performanceMonitor.vk.numRenderPassEndsClearPerFrame.get(),
@@ -220,4 +222,6 @@ void LattePerformanceMonitor_frameBegin()
 	performanceMonitor.vk.numBufferCacheGeometryUniformUploadBytesPerFrame.reset();
 	performanceMonitor.vk.numBufferCachePixelUniformUploadsPerFrame.reset();
 	performanceMonitor.vk.numBufferCachePixelUniformUploadBytesPerFrame.reset();
+	performanceMonitor.vk.numDirectVertexUploadsPerFrame.reset();
+	performanceMonitor.vk.numDirectVertexUploadBytesPerFrame.reset();
 }
